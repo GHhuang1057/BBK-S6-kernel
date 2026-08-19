0da50cbfc76c43123792e010e03c84f63ea3493b
XiKoTaSu <3329989998@qq.com>
2026-08-19 10:58:15 +0800

    techpack: Add tfa98xx and dbmdx drivers

--- techpack/audio/asoc/codecs/dbmdx/Android.mk
+++ techpack/audio/asoc/codecs/dbmdx/Android.mk
@@ -2,18 +2,9 @@
 
 # Assume no targets will be supported
 
-# Check if this driver needs be built for current target
-ifeq ($(call is-board-platform,kona),true)
-AUDIO_SELECT  := CONFIG_SND_SOC_KONA=m
-endif
-
-ifeq ($(call is-board-platform,lito),true)
-AUDIO_SELECT  := CONFIG_SND_SOC_LITO=m
-endif
-
 AUDIO_CHIPSET := audio
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,kona lito),true)
+ifeq ($(call is-board-platform,sdm710),true)
 
 LOCAL_PATH := $(call my-dir)
 
@@ -21,7 +12,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(findstring vendor,$(LOCAL_PATH)),)
 
 ifneq ($(findstring opensource,$(LOCAL_PATH)),)
-	AUDIO_BLD_DIR := $(shell pwd)/vendor/qcom/opensource/audio-kernel
+	AUDIO_BLD_DIR := $(ANDROID_BUILD_TOP)/vendor/qcom/opensource/audio-kernel
 endif # opensource
 
 DLKM_DIR := $(TOP)/device/qcom/common/dlkm
@@ -35,14 +26,14 @@ KBUILD_OPTIONS := AUDIO_ROOT=$(AUDIO_BLD_DIR)
 # requirement we are specifying <chipset>_audio.ko as LOCAL_MODULE.
 # This means we need to rename the module to <chipset>_audio.ko
 # after audio.ko is built.
-KBUILD_OPTIONS += MODNAME=awinic_dlkm
+KBUILD_OPTIONS += MODNAME=dbmdx_dlkm
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 KBUILD_OPTIONS += $(AUDIO_SELECT)
 
 ###########################################################
 include $(CLEAR_VARS)
-LOCAL_MODULE              := $(AUDIO_CHIPSET)_awinic.ko
-LOCAL_MODULE_KBUILD_NAME  := awinic_dlkm.ko
+LOCAL_MODULE              := $(AUDIO_CHIPSET)_dbmdx.ko
+LOCAL_MODULE_KBUILD_NAME  := dbmdx_dlkm.ko
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
